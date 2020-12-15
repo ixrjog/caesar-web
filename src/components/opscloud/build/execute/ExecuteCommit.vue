ee9dd36236e4564095545aefb313255ee8696e81
白衣 <liangjian@xinc818.group>
2020-12-15 13:57:59 +0800

    Commit一经展示不可关闭

--- src/components/opscloud/build/execute/ExecuteCommit.vue
+++ src/components/opscloud/build/execute/ExecuteCommit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-alert title="Commit" type="success" show-icon style="margin-bottom: 5px">
+    <el-alert title="Commit" type="success" show-icon style="margin-bottom: 5px" :closable="false">
       <span>{{commit.commitMessage}}</span>
       <el-button type="text" style="margin-left: 10px; padding: 3px 0" @click="handlerOpenUrl(1)">[点击查看详情]
         {{commit.commit}}
