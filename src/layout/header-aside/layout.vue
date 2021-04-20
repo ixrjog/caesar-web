208ad35a6a6fd93684fc35a696f9fc18b981431b
白衣 <liangjian@xinc818.group>
2021-04-20 11:41:28 +0800

    布局禁用部分组件

--- src/layout/header-aside/layout.vue
+++ src/layout/header-aside/layout.vue
@@ -88,11 +88,11 @@ import d2MenuHeader from './components/menu-header'
 // import d2Tabs from './components/tabs'
 import d2HeaderFullscreen from './components/header-fullscreen'
 import d2HeaderLocales from './components/header-locales'
-import d2HeaderSearch from './components/header-search'
+// import d2HeaderSearch from './components/header-search'
 import d2HeaderSize from './components/header-size'
 import d2HeaderTheme from './components/header-theme'
 import d2HeaderUser from './components/header-user'
-import d2HeaderLog from './components/header-log'
+// import d2HeaderLog from './components/header-log'
 import d2HeaderColor from './components/header-color'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import mixinSearch from './mixins/search'
@@ -107,11 +107,11 @@ export default {
     // d2Tabs,
     d2HeaderFullscreen,
     d2HeaderLocales,
-    d2HeaderSearch,
+    // d2HeaderSearch,
     d2HeaderSize,
     d2HeaderTheme,
     d2HeaderUser,
-    d2HeaderLog,
+    // d2HeaderLog,
     d2HeaderColor
   },
   data () {
